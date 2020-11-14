d42f0877dddbeaa58054be864f45217551f39870
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-11-14 10:56:56 -0500

    fixed travis hopefully

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ require("./routes/api-routes.js")(app);
 db.sequelize.sync().then(() => {
    app.listen(PORT, () => {
       console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
+   });
 });
