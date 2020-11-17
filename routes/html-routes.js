f45ff47f18b117de8db5c0c58c3d44f56bfc4c8e
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-11-16 20:00:23 -0500

    emergency fix

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,15 +8,15 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/account");
+      res.redirect("/account.html");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/mainpage.html"));
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/account");
+      res.redirect("/account.html");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
