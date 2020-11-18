9c0c79f4551191990cf2d7ca01c4f85c7385d31e
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-11-18 16:49:45 -0500

    heroku fix

--- public/js/login.js
+++ public/js/login.js
@@ -1,43 +1,44 @@
-$(document).ready(() => {
-   // Getting references to our form and inputs
-   const loginForm = $("form.login");
-   const emailInput = $("input#email-input");
-   const passwordInput = $("input#password-input");
+const { ConnectionError } = require("sequelize/types");
 
-   // When the form is submitted, we validate there's an email and password entered
-   loginForm.on("submit", (event) => {
-      console.log("Click!");
-      event.preventDefault();
-      const userData = {
-         email: emailInput.val().trim(),
-         password: passwordInput.val().trim()
-      };
+// Getting references to our form and inputs
+const loginForm = $("form.login");
+const emailInput = $("input#email-input");
+const passwordInput = $("input#password-input");
 
-      if (!userData.email || !userData.password) {
-         return;
-      }
+// When the form is submitted, we validate there's an email and password entered
+loginForm.on("submit", (event) => {
+   console.log("Click!");
+   event.preventDefault();
+   const userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim()
+   };
 
-      // If we have an email and password we run the loginUser function and clear the form
-      loginUser(userData.email, userData.password);
-      emailInput.val("");
-      passwordInput.val("");
-   });
-
-   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-   function loginUser(email, password) {
-      // console.log(email);
-      $.post("/api/login", {
-         email: email,
-         password: password
-      })
-         // eslint-disable-next-line no-unused-vars
-         .then((User) => {
-            // console.log(User);
-            window.location.replace("/account");
-         // If there's an error, log the error
-         })
-         .catch((err) => {
-            console.log(err);
-         });
+   if (!userData.email || !userData.password) {
+      return;
    }
+
+   // If we have an email and password we run the loginUser function and clear the form
+   loginUser(userData.email, userData.password);
+   emailInput.val("");
+   passwordInput.val("");
 });
+
+// loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+function loginUser(email, password) {
+   // console.log(email);
+   $.post("/api/login", {
+      email: email,
+      password: password
+   })
+   // eslint-disable-next-line no-unused-vars
+      .then((User) => {
+         // console.log(User);
+         window.location.replace("/account");
+         // If there's an error, log the error
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+}
+
