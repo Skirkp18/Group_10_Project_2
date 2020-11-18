2c2d5f3b57c713e13b4b011c866e4b9c1e02bfc5
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-11-18 16:59:51 -0500

    v1.0.1

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,9 @@
 const { ConnectionError } = require("sequelize/types");
 
-// Getting references to our form and inputs
 const loginForm = $("form.login");
 const emailInput = $("input#email-input");
 const passwordInput = $("input#password-input");
 
-// When the form is submitted, we validate there's an email and password entered
 loginForm.on("submit", (event) => {
    console.log("Click!");
    event.preventDefault();
@@ -18,24 +16,20 @@ loginForm.on("submit", (event) => {
       return;
    }
 
-   // If we have an email and password we run the loginUser function and clear the form
    loginUser(userData.email, userData.password);
    emailInput.val("");
    passwordInput.val("");
 });
 
-// loginUser does a post to our "api/login" route and if successful, redirects us the the members page
 function loginUser(email, password) {
    // console.log(email);
    $.post("/api/login", {
       email: email,
       password: password
    })
-   // eslint-disable-next-line no-unused-vars
       .then((User) => {
          // console.log(User);
          window.location.replace("/account");
-         // If there's an error, log the error
       })
       .catch((err) => {
          console.log(err);
